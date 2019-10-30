3bafe0410ab74ab0a9a0c43fcff9970f1705da41
Frank Poth <frank.poth@1901group.com>
2019-10-30 17:21:17 -0400

    word-puzzle: removed all console.log messages

--- test/pages/games/content/word-puzzle/service-worker.js
+++ test/pages/games/content/word-puzzle/service-worker.js
@@ -3,20 +3,12 @@ const FILES_TO_CACHE = ["manifest.webmanifest", "word-puzzle.css", "word-puzzle.
 
 self.addEventListener("activate", event => {
 
-  console.log("Service Worker Activate Initiated");
-
   event.waitUntil(clients.claim().then(() => {
-
-    console.log("Service Worker Activate: client claimed");
 
     caches.keys().then(keys => Promise.all(keys.map(key => {
 
-      console.log("Service Worker Activate: current cache: " + CACHE_NAME + ", found cache: " + key);
-  
       if (CACHE_NAME !== key) {
-  
-        console.log("Service Worker Activate: deleting old cache: " + key);
-  
+    
         return caches.delete(key);
   
       }
@@ -29,33 +21,23 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
 
-  console.log("Service Worker Fetch Initiated");
-
   event.respondWith(caches.open(CACHE_NAME).then(cache => {
-
-    console.log("Service Worker Fetch: fetch: url: " + event.request.url);
 
     return cache.match(event.request).then( response => {
 
       if (response) {
           
-        console.log("Service Worker Fetch: response: url: " + response.url + " type: " + response.type);
-
         return response;
 
       }
 
       return fetch(event.request).then(network_response => {
 
-        console.log("Service Worker Fetch: response (network): url: " + network_response.url + " type: " + network_response.type);
-
         if (network_response) cache.put(event.request, network_response.clone());
 
         return network_response;
 
       }).catch(error => {
-
-        console.log("Service Worker Fetch: could not find " + event.request.url + " over network");
 
         throw error;
 
@@ -71,12 +53,7 @@ self.addEventListener("install", event => {
 
   self.skipWaiting();
 
-  console.log("Service Worker Install Initiated");
-
   event.waitUntil(caches.open(CACHE_NAME).then(cache => {
-
-    console.log("Service Worker Install: opening cache: " + CACHE_NAME);
-    console.log("Service Worker Install: caching: " + FILES_TO_CACHE);
 
     return cache.addAll(FILES_TO_CACHE);
 
